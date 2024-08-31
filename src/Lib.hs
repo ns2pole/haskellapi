00c6935035c11a20ad4f5320081e1584e8df8d1c
ns2pole <spaceofstar2@gmail.com>
2024-08-31 22:51:32 +0900

    haskellapiテスト

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,6 +13,7 @@ import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
 import Control.Monad.IO.Class (liftIO)
 import Utils (doubleValue) -- 別のファイルから関数をインポート
+import Utils (getGCD) -- 別のファイルから関数をインポート
 
 -- Pointデータ型の定義
 data Point = Point
@@ -25,22 +26,27 @@ instance FromJSON Point
 
 -- 2つの整数を受け取るデータ型の定義
 data TwoInts = TwoInts
-  { a :: Int
-  , b :: Int
+  { int1 :: Int
+  , int2 :: Int
   } deriving (Eq, Show, Generic)
 
 instance FromJSON TwoInts
 
--- APIタイプの定義
+-- APIタイプの定義に新しいエンドポイントを追加
 type API = "doublePoint" :> ReqBody '[JSON] TwoInts :> Post '[JSON] Point
-
--- サーバーの定義
-server :: Server API
-server = doublePointHandler
+       :<|> "getGCD" :> ReqBody '[JSON] TwoInts :> Post '[JSON] Int
 
 -- リクエストを処理するハンドラー
 doublePointHandler :: TwoInts -> Handler Point
 doublePointHandler (TwoInts a b) = return $ Point (doubleValue a) (doubleValue b)
+
+-- 新しいハンドラーを追加
+getGCDHandler :: TwoInts -> Handler Int
+getGCDHandler (TwoInts int1 int2) = return (getGCD int1 int2)
+
+-- サーバーの定義を更新
+server :: Server API
+server = doublePointHandler :<|> getGCDHandler
 
 -- サーバーの設定
 api :: Proxy API
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,6 +1,18 @@
 -- Utils.hs
-module Utils (doubleValue) where
+module Utils (doubleValue, getGCD) where
 
 -- 2倍する処理を行う関数
 doubleValue :: Int -> Int
 doubleValue n = 200 * n + 12
+
+
+
+getGCD :: Int -> Int -> Int
+getGCD a b = a + b
+
+-- コードを書く
+-- localで動く
+-- localでangularから動く
+-- centosにhaskellを送る→build→buildしたものをmacに送る→gitにpush→vpsでgitでpull→execute
+
+
