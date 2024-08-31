1935235fc702dba3333be9dc47f3bdc52eb1750a
ns2pole <spaceofstar2@gmail.com>
2024-09-01 01:25:02 +0900

    centos版をbuild

--- src/Utils.hs
+++ src/Utils.hs
@@ -14,5 +14,5 @@ getGCD a b = a `mod` b
 -- localで動く
 -- localでangularから動く
 -- centosにhaskellを送る→build→buildしたものをmacに送る→gitにpush→vpsでgitでpull→executeファイルを/usr/local/binに移動→execute
-
-
+-- docker cp ~/Program/haskell_exercise/first-project d502e9d8db1a:/app/
+-- docker cp d502e9d8db1a:/app/first-project/.stack-work ~
